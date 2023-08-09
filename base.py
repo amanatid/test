361af560c0347ca4fe40dd093818e16297f21a94
amanatid <116643953+amanatid@users.noreply.github.com>
2023-08-09 20:07:52 +0300

    Update base.py

--- base.py
+++ base.py
@@ -13,16 +13,3 @@ from llama_index import SimpleDirectoryReader
 #################
 from deta import Deta
 
-class ArxivReader_mod(BaseReader):
-    """Arxiv Reader.
-
-    Gets a search query, return a list of Documents of the top corresponding scientific papers on Arxiv.
-    """
-
-    def __init__(
-            self,
-    ):
-        """Initialize with parameters."""
-        super().__init__()
-
- 
