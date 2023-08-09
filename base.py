ec7670bf8cc5e5cdf1fbc56b80606a656b0fca63
amanatid <116643953+amanatid@users.noreply.github.com>
2023-08-09 20:05:45 +0300

    Update base.py

--- base.py
+++ base.py
@@ -25,27 +25,4 @@ class ArxivReader_mod(BaseReader):
         """Initialize with parameters."""
         super().__init__()
 
-    def _hacky_hash(self, some_string):
-        _hash = hashlib.md5(some_string.encode("utf-8")).hexdigest()
-        return _hash
-
-    def load_data(
-            self,
-            search_query: str,
-            max_results: Optional[int] = 50,
-            search_criterion: Optional[int] = 0,
-    ) -> List[Document]:
-        """Search for a topic on Arxiv, download the PDFs of the top results locally, then read them.
-
-        Args:
-            search_query (str): A topic to search for (e.g. "Artificial Intelligence").
-            papers_dir (Optional[str]): Locally directory to store the papers
-            max_results (Optional[int]): Maximum number of papers to fetch.
-
-        Returns:
-            List[Document]: A list of Document objects.
-        """
-
-      
-
-        return arxiv_documents + abstract_documents
+ 
